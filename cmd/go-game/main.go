92001692a06379d3f419e210d672125e30d80321
ArtyomKr <tyomik.krasnitsky@gmail.com>
2025-01-06 21:06:22 +0300

    feat: add basic game window logic

---
+++ cmd/go-game/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	rl "github.com/gen2brain/raylib-go/raylib"
+	cm "go-game/internal/camera"
+)
+
+func main() {
+	screenWidth := int32(800)
+	screenHeight := int32(450)
+
+	rl.InitWindow(screenWidth, screenHeight, "Strategy Game")
+	defer rl.CloseWindow()
+
+	camera := cm.InitCamera()
+	cameraControls := cm.NewCameraControls()
+
+	cubePosition := rl.NewVector3(0, 0, 0)
+
+	rl.SetTargetFPS(60)
+
+	for !rl.WindowShouldClose() {
+		// Update camera based on controls
+		cm.UpdateCamera(&camera, &cameraControls)
+
+		rl.BeginDrawing()
+
+		rl.ClearBackground(rl.RayWhite)
+
+		rl.BeginMode3D(camera)
+
+		rl.DrawCube(cubePosition, 5.0, 5.0, 5.0, rl.Red)
+		rl.DrawCubeWires(cubePosition, 5.0, 5.0, 5.0, rl.Maroon)
+
+		rl.DrawGrid(1000, 100.0)
+
+		rl.EndMode3D()
+
+		rl.DrawText("WASD/Arrows: Move Camera\nQ/E: Rotate Camera\nR/F: Adjust Height\nMiddle Mouse: Rotate\nRight Mouse: Pan",
+			10, 40, 20, rl.DarkGray)
+
+		rl.DrawText(fmt.Sprintf("Camera position: X:%v Y:%v Z: %v", camera.Position.X, camera.Position.Y, camera.Position.Z), 10, 200, 20, rl.DarkGray)
+		rl.DrawFPS(10, 10)
+
+		rl.EndDrawing()
+	}
+}
